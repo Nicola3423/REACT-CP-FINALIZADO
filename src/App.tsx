ccf7e2556eb22093cd131011cbb82cf8b31788c8
Nicola3423 <nnmmontecravo@gmail.com>
2024-04-17 22:39:13 -0300

    terceiro commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+// Importações necessárias
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import Home from "./componentes/Home";
 import Posts from "./componentes/Posts";
 import Users from "./componentes/Users";
 import Photos from "./componentes/Photos";
-import img1 from "./componentes/img/FIAP.jpg";
-import img2 from "./componentes/img/React.png";
+import img1 from "./componentes/img/FIAP.jpg"; // Importa a imagem para o primeiro ícone
+import img2 from "./componentes/img/React.png"; // Importa a imagem para o segundo ícone
 
 function App() {
   return (
@@ -15,20 +16,25 @@ function App() {
           <h1 className="text-3xl">CP 02 - 2° SEMESTRE</h1>
         </header>
         
-        <nav className="flex justify-between py-3"> {/* Alterado justify-center para justify-between */}
+        {/* Navegação com ícones e links */}
+        <nav className="flex justify-between py-3">
           <ul className="flex space-x-4">
             <li><Link to='/' className="text-blue-500 hover:text-blue-700">HOME</Link></li>
             <li><Link to='/Posts' className="text-blue-500 hover:text-blue-700">Posts</Link></li>
             <li><Link to='/Users' className="text-blue-500 hover:text-blue-700">Users</Link></li>
             <li><Link to='/Photos' className="text-blue-500 hover:text-blue-700">Fotos</Link></li>
           </ul>
-          <div className="flex"> {/* Criando uma div para as imagens */}
-            <img src={img1} alt="Image 1" className="w-16 h-16 mr-2" /> {/* Adicione a classe para ajustar o tamanho da imagem */}
-            <img src={img2} alt="Image 2" className="w-16 h-16 mr-2" /> {/* Adicione a classe para ajustar o tamanho da imagem */}
+          {/* Div para os ícones */}
+          <div className="flex">
+            {/* Primeiro ícone */}
+            <img src={img1} alt="Image 1" className="w-16 h-16 mr-2" />
+            {/* Segundo ícone */}
+            <img src={img2} alt="Image 2" className="w-16 h-16 mr-2" />
           </div>
         </nav>
         
         <main>
+          {/* Rotas para os componentes */}
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/Posts" element={<Posts />} />
@@ -38,7 +44,8 @@ function App() {
         </main>
         
         <footer className="text-center py-4">
-          <p>Nicola Monte Cravo Garofalo, RM 553991, 1TDSZ, Link do repositorio: https://github.com/Nicola3423/REACT-CP-FINALIZADO</p>
+          {/* Informações do autor e link para o repositório */}
+          <p>Nicola Monte Cravo Garofalo, RM 553991, 1TDSZ, Link do repositório: https://github.com/Nicola3423/REACT-CP-FINALIZADO</p>
         </footer>
       </div>
     </Router>
@@ -46,3 +53,4 @@ function App() {
 }
 
 export default App;
+
